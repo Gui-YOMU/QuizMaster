c60aef8bfc2d95455b62d1d36f566f2c53c706b8
Gui-YOMU <rallo.guillaumemaurice@gmail.com>
2026-06-18 11:06:30 +0200

    Modification url socket

--- QuizMaster_frontend/src/presentation/contexts/GameContext.tsx
+++ QuizMaster_frontend/src/presentation/contexts/GameContext.tsx
@@ -11,24 +11,24 @@ export const GameProvider = ({ children }: { children: React.ReactNode }) => {
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    const newSocket = io(import.meta.env.VITE_API_URL);
+    const newSocket = io(import.meta.env.VITE_SOCKET_URL, {
+      transports: ["websocket"],
+      path: "/socket.io",
+    });
     setSocket(newSocket);
 
     return () => {
-        newSocket.disconnect();
-    }
-  }, [])
+      newSocket.disconnect();
+    };
+  }, []);
 
   return (
-    <GameContext.Provider value={{ socket }}>
-      {children}
-    </GameContext.Provider>
+    <GameContext.Provider value={{ socket }}>{children}</GameContext.Provider>
   );
-}
+};
 
 export function useGame() {
   const ctxt = useContext(GameContext);
-  if (!ctxt) throw new Error("La fonction useGame a besoin d'un provider.")
+  if (!ctxt) throw new Error("La fonction useGame a besoin d'un provider.");
   return ctxt;
 }
-
