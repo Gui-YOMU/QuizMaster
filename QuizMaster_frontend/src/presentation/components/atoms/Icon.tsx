ffa7fa7837db151abd808a1d61eb3df60ba6379e
Gui-YOMU <rallo.guillaumemaurice@gmail.com>
2026-06-21 13:36:36 +0200

    Responsive navbar

--- QuizMaster_frontend/src/presentation/components/atoms/Icon.tsx
+++ QuizMaster_frontend/src/presentation/components/atoms/Icon.tsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router";
+
 export const Icon = () => {
   return (
-    <div className="h-full">
-      <img
-        className="w-full h-full object-contain"
-        src="/images/Icon_color_bordered.png"
-        alt="Icon"
-      />
-    </div>
+    <>
+      <Link to={"/playerDashboard"}>
+        <div className="h-full">
+          <img
+            className="w-full h-full object-contain"
+            src="/images/Icon_color_bordered.png"
+            alt="Icon"
+          />
+        </div>
+      </Link>
+    </>
   );
-}
+};
--- QuizMaster_frontend/src/presentation/components/molecules/NavBar.tsx
+++ QuizMaster_frontend/src/presentation/components/molecules/NavBar.tsx
@@ -1,17 +1,39 @@
-import { Gamepad2, LogOut, Palette, User } from 'lucide-react';
-import { NavButton } from '../atoms/NavButton.tsx';
+import { Gamepad2, LogOut, Palette, User } from "lucide-react";
+import { NavButton } from "../atoms/NavButton.tsx";
 
 interface NavBarProps {
-    role: string | undefined;
+  role: string | undefined;
 }
 
 export const NavBar = ({ role }: NavBarProps) => {
-    return (
-        <div className="flex justify-center gap-5">
-            {role === 'creator' && <NavButton to='/creatorDashboard' icon={<Palette size={40} color='white'/>} bgColor="bg-maingold" />}
-            <NavButton to='/playerDashboard' icon={<Gamepad2 size={40} color='white'/>} bgColor="bg-maingold" />
-            <NavButton to='/profile' icon={<User size={40} color='white'/>} bgColor="bg-maingold" />
-            <NavButton to='/logout' icon={<LogOut size={40} color='white'/>} bgColor="bg-error" />
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center gap-5">
+      <div className="hidden lg:block">
+        {role === "creator" && (
+          <NavButton
+            to="/creatorDashboard"
+            icon={<Palette size={40} color="white" />}
+            bgColor="bg-maingold"
+          />
+        )}
+      </div>
+      <div className="hidden lg:block">
+        <NavButton
+          to="/playerDashboard"
+          icon={<Gamepad2 size={40} color="white" />}
+          bgColor="bg-maingold"
+        />
+      </div>
+      <NavButton
+        to="/profile"
+        icon={<User size={40} color="white" />}
+        bgColor="bg-maingold"
+      />
+      <NavButton
+        to="/logout"
+        icon={<LogOut size={40} color="white" />}
+        bgColor="bg-error"
+      />
+    </div>
+  );
+};
