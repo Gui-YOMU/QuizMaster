18fe9b6b1127628acf55659f9643daf7e6dea572
Gui-YOMU <rallo.guillaumemaurice@gmail.com>
2026-06-23 17:04:41 +0200

    Modification init

--- init.sql
+++ init.sql
@@ -1,2 +1,7 @@
-GRANT ALL PRIVILEGES ON quizmaster_db.* TO 'quizmaster'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'quizmaster'@'%' IDENTIFIED BY 'password';
+
+ALTER USER 'quizmaster'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+
+GRANT ALL PRIVILEGES ON quizmaster_db.* TO 'quizmaster'@'%';
+
 FLUSH PRIVILEGES;
